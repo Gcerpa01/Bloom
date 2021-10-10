e33354f5deffe2684aa3f613887750d8372657ee
Gerardo <gcerpa02@cox.net>
2021-10-10 16:26:50 -0700

    Initial Commit

---
+++ Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by Gerardo on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
